7d5da0f2eeff3896337a4c28478573db8e10a4e3
jvander- <jvander-@student.s19.be>
2022-03-17 11:38:28 +0100

    add struct ray tracking

--- cub3d.h
+++ cub3d.h
@@ -3,22 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rgelin <rgelin@student.s19.be>             +#+  +:+       +#+        */
+/*   By: jvander- <jvander-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 17:30:35 by rgelin            #+#    #+#             */
-/*   Updated: 2022/03/15 17:04:07 by rgelin           ###   ########.fr       */
+/*   Updated: 2022/03/17 11:37:55 by jvander-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <math.h>
-#include "./minilibx/mlx.h"
-#include "./libft/libft.h"
+#ifndef CUB3D_H
+# define CUB3D_H
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <math.h>
+# include "./minilibx/mlx.h"
+# include "./libft/libft.h"
 
-typedef struct	s_data
+typedef struct s_data
 {
 	char	**map;
 	char	*NO_texture_path;
@@ -38,6 +40,34 @@ typedef struct s_mlx
 	t_data	*data;
 }	t_mlx;
 
+typedef struct s_ray
+{
+	double		posx;
+	double		posy;
+	double		dirx;
+	double		diry;
+	double		planx;
+	double		plany;
+	double		raydirx;
+	double		raydiry;
+	double		camerax;
+	int			mapx;
+	int			mapy;
+	double		sidedistx;
+	double		sidedisty;
+	double		deltadistx;
+	double		deltadisty;
+	int			stepx;
+	int			stepy;
+	int			hit;
+	int			side;
+	double		perpwalldist;
+	int			lineheight;
+	int			drawstart;
+	int			drawend;
+	int			x;
+}					t_ray;
+
 //============ERRORS============//
 
 int		ft_perror(char *msg);
@@ -53,3 +83,4 @@ void	read_file(char *file_path, t_data *data);
 void	get_split_data(char **str);
 void	split_data(t_data *data);
 
+#endif
